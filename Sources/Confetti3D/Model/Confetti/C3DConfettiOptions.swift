0708281d3a131a0e7ae0f661acd425ab20a707b3
Maxime Daymard <mdaymard@gmail.com>
2025-04-14 15:32:50 +0200

    [CLEAN] Indent

--- Sources/Confetti3D/Model/Confetti/C3DConfettiOptions.swift
+++ Sources/Confetti3D/Model/Confetti/C3DConfettiOptions.swift
@@ -29,30 +29,30 @@ public struct C3DConfettiOptions: Sendable {
 
 extension C3DConfettiOptions {
     public static let defaultOptions = C3DConfettiOptions(confetti: Constants.defaultConfetti,
-                                                         isAffectedByGravity: true,
-                                                         birthRate: 40,
-                                                         emissionDuration: 3,
-                                                         lifeSpan: 10)
+                                                          isAffectedByGravity: true,
+                                                          birthRate: 40,
+                                                          emissionDuration: 3,
+                                                          lifeSpan: 10)
 }
 
 fileprivate enum Constants {
     static let defaultConfetti = [
         C3DConfetti(image: UIImage(named: "confetti_01", in: .module, with: nil)!,
-                   color: .blue),
+                    color: .blue),
         C3DConfetti(image: UIImage(named: "confetti_02", in: .module, with: nil)!,
-                   color: .green),
+                    color: .green),
         C3DConfetti(image: UIImage(named: "confetti_03", in: .module, with: nil)!,
-                   color: .yellow),
+                    color: .yellow),
         C3DConfetti(image: UIImage(named: "confetti_04", in: .module, with: nil)!,
-                   color: .purple),
+                    color: .purple),
         C3DConfetti(image: UIImage(named: "confetti_05", in: .module, with: nil)!,
-                   color: .red),
+                    color: .red),
         C3DConfetti(image: UIImage(named: "confetti_06", in: .module, with: nil)!,
-                   color: .cyan),
+                    color: .cyan),
         C3DConfetti(image: UIImage(named: "confetti_07", in: .module, with: nil)!,
-                   color: UIColor(red: 1, green: 0.1, blue: 0, alpha: 1)), // Orange
+                    color: UIColor(red: 1, green: 0.1, blue: 0, alpha: 1)), // Orange
         C3DConfetti(image: UIImage(named: "confetti_08", in: .module, with: nil)!,
-                   color: UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)), // Gold
+                    color: UIColor(red: 1, green: 0.2, blue: 0, alpha: 1)), // Gold
     ]
 }
 
